112cf91bf43b56fba36241105732af5b584a1701
Rusewww <rusewww1@gmail.com>
2022-02-02 14:37:50 +0200

    stic

--- client/src/client.c
+++ client/src/client.c
@@ -1,29 +1,22 @@
 #include "client.h"
 
-static gboolean is_valid(gint argc) {
-    if (argc != 3) {
-        g_printerr("Usage: ./uchat <ip,domen> <port>\n");
-        return FALSE;
-    }
-    return TRUE;
-}
-
 int main(int argc, char **argv) {
     GSocketConnection *connection = NULL;
     GSocketClient *client = g_socket_client_new();
     t_chat *chat = NULL;
 
     if (chdir(MX_CLIENT)) {
-        mx_logger(MX_LOG_FILE, G_LOG_LEVEL_ERROR,
-                  "No working directory mx_client");
+        mx_logger(MX_LOG_FILE, G_LOG_LEVEL_ERROR, "No working directory mx_client");
     }MX_ERRMSG_INCCRDATA;
     if (g_mkdir_with_parents(MX_FILES_DIR, 0755)) {
-        mx_logger(MX_LOG_FILE, G_LOG_LEVEL_ERROR,
-                  "No files directory mx_files_dir");
+        mx_logger(MX_LOG_FILE, G_LOG_LEVEL_ERROR, "No files directory mx_files_dir");
     }
 
-    if (!is_valid(argc))
+    if (argc != 3) {
+        g_printerr("Usage: ./uchat <ip,domen> <port>\n");
         return -1;
+    }
+
     connection = g_socket_client_connect_to_host(
         client, argv[1], g_ascii_strtoll(argv[2], NULL, 10), NULL, NULL);
     if (!connection) {
