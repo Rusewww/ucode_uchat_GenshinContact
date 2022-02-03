8e5175ad7057c4536f886738bf0d59f24ba3519b
Rusewww <rusewww1@gmail.com>
2022-02-03 05:57:29 +0200

    css

--- client/src/gui/mx_msgcreate_box_energy.c
+++ client/src/gui/mx_msgcreate_box_energy.c
@@ -1,8 +1,5 @@
 #include "client.h"
 
 void mx_msgcreate_box_energy(GtkWidget *box_status, t_gmsg *gmsg) {
-    GtkWidget *box_energy = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 0);
-    GtkWidget *img = gtk_image_new_from_icon_name(" ",
-                                                  GTK_ICON_SIZE_MENU);
-
+    if (box_status && gmsg);
 }
