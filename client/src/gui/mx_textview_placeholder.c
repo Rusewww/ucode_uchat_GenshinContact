4b287c2fbbca9a440c2af7a4041afa1a95b1f9b3
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 19:42:44 +0200

    css_connection

--- client/src/gui/mx_textview_placeholder.c
+++ client/src/gui/mx_textview_placeholder.c
@@ -1,12 +1,10 @@
 #include "../../inc/client.h"
 
-static gboolean set_placeholder(GtkWidget *textview, GdkEvent *event,
-                                   t_chat *chat) {
-    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
+static gboolean set(GtkWidget *text_view, GdkEvent *event, t_chat *chat) {
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
     const gchar *placeholder = "Enter your message";
     GtkTextIter start;
     GtkTextIter end;
-
     gtk_text_buffer_get_start_iter(buffer, &start);
     gtk_text_buffer_get_end_iter(buffer, &end);
     if (strlen(gtk_text_buffer_get_text(buffer, &start, &end, FALSE)) == 0) {
@@ -18,17 +16,13 @@ static gboolean set_placeholder(GtkWidget *textview, GdkEvent *event,
 }
 
 void mx_connect_set_placeholder(t_chat *chat) {
-    GObject *textview = gtk_builder_get_object(chat->builder, "msg_entry");
-
-    g_signal_connect(GTK_TEXT_VIEW(textview), "focus-out-event",
-                     G_CALLBACK(set_placeholder), chat);
+    GObject *text_view = gtk_builder_get_object(chat->builder, "msg_entry");
+    g_signal_connect(GTK_TEXT_VIEW(text_view), "focus-out-event", G_CALLBACK(set), chat);
 }
 
-static gboolean unset_placeholder(GtkWidget *textview, GdkEvent  *event,
-                                   t_chat *chat) {
-    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
+static gboolean unset(GtkWidget *text_view, GdkEvent  *event, t_chat *chat) {
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer(GTK_TEXT_VIEW(text_view));
     const gchar *placeholder = "";
-    
     if (chat->msg_placeholder) {
         gtk_text_buffer_set_text(buffer, placeholder, -1);
         chat->msg_placeholder = FALSE;
@@ -38,8 +32,6 @@ static gboolean unset_placeholder(GtkWidget *textview, GdkEvent  *event,
 }
 
 void mx_connect_unset_placeholder(t_chat *chat) {
-    GObject *textview = gtk_builder_get_object(chat->builder, "msg_entry");
-
-    g_signal_connect(GTK_TEXT_VIEW(textview), "focus-in-event",
-                     G_CALLBACK(unset_placeholder), chat);
+    GObject *text_view = gtk_builder_get_object(chat->builder, "msg_entry");
+    g_signal_connect(GTK_TEXT_VIEW(text_view), "focus-in-event", G_CALLBACK(unset), chat);
 }
