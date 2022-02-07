2590fd167bcf2735d6e4eeb21d0d27dce2010781
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 15:29:13 +0200

    mx_delete_row_room

--- client/src/gui/mx_delete_row_room.c
+++ client/src/gui/mx_delete_row_room.c
@@ -1,20 +1,23 @@
 #include "../../inc/client.h"
 
-void mx_unselect_room(t_groom *groom, GtkBuilder *builder) {
-    mx_set_default_room_sett(builder);
-    if (groom->page)
+void mx_unselect_room(t_groom *groom, GtkBuilder *build) {
+    mx_set_default_room_sett(build);
+    if (groom->page) {
         gtk_widget_hide(GTK_WIDGET(groom->page));
-    if (groom->row_room)
+    }
+    if (groom->row_room) {
         gtk_widget_hide(GTK_WIDGET(groom->row_room));
-    mx_set_room_widgets_visibility(builder, FALSE);
+    }
+    mx_set_room_widgets_visibility(build, FALSE);
 }
 
-void mx_delete_row_room(GtkListBoxRow *row, GtkBuilder *builder) {
+void mx_delete_row_room(GtkListBoxRow *row, GtkBuilder *build) {
     t_groom *groom = g_object_get_data(G_OBJECT(row), "groom");
-
-    if (gtk_list_box_row_is_selected(row))
-        mx_unselect_room(groom, builder);
-    if (groom->page)
+    if (gtk_list_box_row_is_selected(row)) {
+        mx_unselect_room(groom, build);
+    }
+    if (groom->page) {
         gtk_widget_destroy(GTK_WIDGET(groom->page));
+    }
     gtk_widget_destroy(GTK_WIDGET(row));
 }
