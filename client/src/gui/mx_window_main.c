1729a88c7f1acfb8b6b786de2f2e69fe7ebec5ee
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 19:10:35 +0200

    mx_window_main

--- client/src/gui/mx_window_main.c
+++ client/src/gui/mx_window_main.c
@@ -2,29 +2,20 @@
 
 GtkBuilder *mx_init_window(gint argc, char **argv) {
     GtkBuilder *builder;
-
     gtk_init(&argc, &argv);
     builder = gtk_builder_new();
-
     if (gtk_builder_add_from_file(builder, MX_GUI_PATH, NULL) == 0) {
         mx_logger(MX_LOG_FILE, G_LOG_LEVEL_ERROR, "Gui.glade is missing(xml file).");
     }
-
     gtk_builder_connect_signals(builder, builder);
     return builder;
 }
 
-static void change_icontheme_paths(GtkBuilder *builder) {
-    GObject *desc = gtk_builder_get_object(builder, "buffer_message");
-    const gchar *path[] = {"../src/gui/resources",
-                           "../src/gui/resources/stickers"};
-
+gint mx_start_gui(t_chat *chat) {
+    GObject *desc = gtk_builder_get_object(chat->builder, "buffer_message");
+    const gchar *path[] = {"../src/gui/resources", "../src/gui/resources/stickers"};
     gtk_icon_theme_set_search_path(gtk_icon_theme_get_default(), path, 2);
     gtk_text_buffer_set_text(GTK_TEXT_BUFFER(desc), "Enter your message", -1);
-}
-
-gint mx_start_gui(t_chat *chat) {
-    change_icontheme_paths(chat->builder);
     gtk_main();
     return 0;
 }
