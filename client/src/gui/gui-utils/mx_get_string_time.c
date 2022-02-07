c43980b912eb1949118660e32d85d1b6acde402c
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 21:34:49 +0200

    is_file_image

--- client/src/gui/gui-utils/mx_get_string_time.c
+++ client/src/gui/gui-utils/mx_get_string_time.c
@@ -4,9 +4,10 @@ gchar *mx_get_string_time(guint64 miliseconds, gint8 format) {
     GDateTime *dt = g_date_time_new_from_unix_utc(miliseconds / 1000);
     GDateTime *new = g_date_time_to_local(dt);
 
-    if (format == MX_TIME_SHORT)
+    if (format == MX_TIME_SHORT) {
         return g_date_time_format(new, "%H:%M");
-    else
+    } else {
         return g_date_time_format(new, "%d.%m.%Y    %H:%M:%S");
+    }
 }
 
--- client/src/gui/gui-utils/mx_is_file_image.c
+++ client/src/gui/gui-utils/mx_is_file_image.c
@@ -1,19 +1,18 @@
 #include "client.h"
 
 gboolean mx_is_file_image(gchar *filename) {
-    if (g_str_has_suffix(filename, ".png")
-        || g_str_has_suffix(filename, ".jpg")
-        || g_str_has_suffix(filename, ".jpeg")) {
+    if (g_str_has_suffix(filename, ".png") || g_str_has_suffix(filename, ".jpg") ||
+        g_str_has_suffix(filename, ".jpeg")) {
         return TRUE;
-    }
-    else
+    } else {
         return FALSE;
+    }
 }
 
 gboolean mx_is_file_animation(gchar *filename) {
     if (g_str_has_suffix(filename, ".gif")) {
         return TRUE;
-    }
-    else
+    } else {
         return FALSE;
+    }
 }
