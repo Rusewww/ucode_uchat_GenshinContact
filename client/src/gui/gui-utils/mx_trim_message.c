63814150bf170ba8c5f76c8c8c937b01a34bebf6
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 22:46:17 +0200

    fix

--- client/src/gui/gui-utils/mx_trim_message.c
+++ client/src/gui/gui-utils/mx_trim_message.c
@@ -2,7 +2,6 @@
 
 void mx_trim_message(gchar **message) {
     gchar *trimed = NULL;
-
     if (strlen(*message) > MX_MAX_MESSAGE) {
         trimed = g_strndup(*message, MX_MAX_MESSAGE);
         g_free(*message);
--- client/src/gui/gui-utils/send_message_handlers.c
+++ client/src/gui/gui-utils/send_message_handlers.c
@@ -1,28 +1,27 @@
 #include "client.h"
 
-void mx_send_message_handle_enter(GtkTextView *textview,
-                                  GdkEvent *event, t_chat *chat) {
+void mx_send_message_handle_enter(GtkTextView *textview, GdkEvent *event, t_chat *chat) {
     GtkTextBuffer *buffer = gtk_text_view_get_buffer(textview);
-
-    if (event->key.keyval == MX_KEY_SHIFT)
+    if (event->key.keyval == MX_KEY_SHIFT) {
         chat->shift_hold = TRUE;
+    }
     g_usleep(0);
     if (event->key.keyval == MX_KEY_ENTER) {
-        if (chat->shift_hold)
+        if (chat->shift_hold) {
             gtk_text_buffer_insert_at_cursor(buffer, "\n", -1);
-        else if (!mx_widget_is_visible("box_editing_msg", chat->builder)){
+        } else if (!mx_widget_is_visible("box_editing_msg", chat->builder)) {
             mx_req_send_message(NULL, chat);
             mx_clear_buffer_text("buffer_message", chat->builder);
-        }
-        else
+        } else {
             mx_req_edit_message(NULL, chat);
+        }
     }
 }
 
-void mx_send_message_handle_shift(GtkWidget *textview,
-                                  GdkEvent *event, t_chat *chat) {
-    if (event->key.keyval == MX_KEY_SHIFT)
+void mx_send_message_handle_shift(GtkWidget *textview, GdkEvent *event, t_chat *chat) {
+    if (event->key.keyval == MX_KEY_SHIFT) {
         chat->shift_hold = FALSE;
-    (void)textview;
+    }
+    (void) textview;
 }
 
