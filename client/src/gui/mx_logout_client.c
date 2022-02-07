eea2e4e6b22a087e01dc851d6d86d23837ec5b64
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 15:36:54 +0200

    mx_messages_top_reached

--- client/src/gui/mx_logout_client.c
+++ client/src/gui/mx_logout_client.c
@@ -1,10 +1,8 @@
 #include "../../inc/client.h"
 
 void mx_logout_client(t_chat *chat) {
-    GtkListBox *box = GTK_LIST_BOX(gtk_builder_get_object(chat->builder,
-                                                          "listbox_rooms"));
+    GtkListBox *box = GTK_LIST_BOX(gtk_builder_get_object(chat->builder, "listbox_rooms"));
     GtkListBoxRow *row = gtk_list_box_get_row_at_index(box, 0);
-
     mx_start_main_window(chat);
     while (row) {
         mx_delete_row_room(row, chat->builder);
