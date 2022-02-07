bae51b062a57a54e30022bbcf3af38ef0f76ce3f
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 13:42:11 +0200

    create_message

--- client/src/gui/create_message.c
+++ client/src/gui/create_message.c
@@ -1,34 +1,38 @@
 #include "client.h"
 
-static gboolean get_data(cJSON *msg, cJSON **data, char *field, 
-                         cJSON_bool(check_valid)(const cJSON *const)) {
+static gboolean get_data(cJSON *msg, cJSON **data, char *field, cJSON_bool(check_valid)(const cJSON *const)) {
     *data = cJSON_GetObjectItemCaseSensitive(msg, field);
-    if (!*data || !check_valid(*data))
+    if (!*data
+        || !check_valid(*data)) {
         return FALSE;
+    }
     return TRUE;
 }
 
-static t_groom *help_create_message(gboolean *valid, cJSON *msg, t_gmsg *gmsg,
-                                    t_chat *chat) {
+static t_groom *help_create_message(gboolean *valid, cJSON *msg, t_gmsg *gmsg, t_chat *chat) {
     cJSON *data = NULL;
     t_groom *groom = NULL;
 
-    if (*valid && (*valid = get_data(msg, &data, "message", cJSON_IsString)))
+    if (*valid && (*valid = get_data(msg, &data, "message", cJSON_IsString))) {
         gmsg->msg = strdup(data->valuestring);
-    if (*valid && (*valid = get_data(msg, &data, "room_id", cJSON_IsNumber)))
+    }
+    if (*valid && (*valid = get_data(msg, &data, "room_id", cJSON_IsNumber))) {
         gmsg->room_id = data->valuedouble;
-    if (!(*valid))
+    }
+    if (!(*valid)) {
         return NULL;
+    }
     groom = mx_get_groom_by_id(data->valuedouble, chat->builder);
-    if (!groom)
+    if (!groom) {
         return NULL;
+    }
     if (*valid && (*valid = get_data(msg, &data, "user_id", cJSON_IsNumber))) {
-        gpointer hash = g_hash_table_lookup(groom->members,
-                                            GINT_TO_POINTER(data->valueint));
+        void *hash = g_hash_table_lookup(groom->members, GINT_TO_POINTER(data->valueint));
         gmsg->login = g_strdup(MX_J_STR(hash));
     }
-    if (*valid && (*valid = get_data(msg, &data, "date", cJSON_IsNumber)))
+    if (*valid && (*valid = get_data(msg, &data, "date", cJSON_IsNumber))) {
         gmsg->date = data->valuedouble;
+    }
     return groom;
 }
 
@@ -37,23 +41,30 @@ t_gmsg *mx_create_gmsg(cJSON *msg, t_chat *chat) {
     cJSON *data = NULL;
     gboolean valid = TRUE;
     help_create_message(&valid, msg, gmsg, chat);
-    if (valid && (valid = get_data(msg, &data, "message_id", cJSON_IsNumber)))
-        gmsg->message_id = data->valuedouble;
-    if (valid && (valid = get_data(msg, &data, "msg_type", cJSON_IsNumber)))
-        gmsg->type = data->valuedouble;
-    if (valid && (valid = get_data(msg, &data, "power", cJSON_IsNumber)))
-        gmsg->power = mx_get_used_power(data->valuedouble);
-    if (!valid && mx_delete_gmsg(gmsg))
-        return NULL;
+    if (valid) {
+        if ((valid = get_data(msg, &data, "message_id", cJSON_IsNumber)) == 1) {
+            gmsg->message_id = data->valuedouble;
+        }
+        if ((valid = get_data(msg, &data, "msg_type", cJSON_IsNumber)) == 1) {
+            gmsg->type = data->valuedouble;
+        }
+        if ((valid = get_data(msg, &data, "power", cJSON_IsNumber)) == 1) {
+            gmsg->power = 0.026751 * data->valuedouble;
+        }
+    } else {
+        if (mx_delete_gmsg(gmsg)) {
+            return NULL;
+        }
+    }
     gmsg->row_msg = NULL;
     return gmsg;
 }
 
 gboolean mx_delete_gmsg(t_gmsg *gmsg) {
     if (gmsg) {
-        mx_free((void**)&(gmsg->msg));
-        mx_free((void**)&(gmsg->login));
-        mx_free((void**)&gmsg);
+        mx_free((void **) &(gmsg->msg));
+        mx_free((void **) &(gmsg->login));
+        mx_free((void **) &gmsg);
         gmsg = NULL;
     }
     return TRUE;
