bbba6e1c06899efdd3001a8d28a8d62e6958b25a
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 21:02:48 +0200

    connect_search_callbacks

--- client/src/gui/gui-utils/connect_search_callbacks.c
+++ client/src/gui/gui-utils/connect_search_callbacks.c
@@ -1,54 +1,43 @@
 #include "client.h"
 
-static void req_search_global_rooms(gchar *search_name, t_chat *chat) {
-    t_dtp *dtp = mx_search_rooms_request(search_name);
-
-    mx_send(chat->out, dtp);
-    mx_free_request(&dtp);
-}
-
 void mx_start_search_room(GtkSearchEntry *sentry, t_chat *chat) {
-    gchar *search_name = (gchar*)gtk_entry_get_text(GTK_ENTRY(sentry));
-    t_filter_data *filter_data = mx_create_filter_data(search_name);
-
+    gchar *search = (gchar *) gtk_entry_get_text(GTK_ENTRY(sentry));
+    t_filter_data *filter_data = mx_create_filter_data(search);
     mx_clear_global_search(chat->builder);
     mx_search_delim_set_visibility(chat->builder, TRUE);
-    if (!strlen(search_name))
+    if (!strlen(search)) {
         mx_stop_search_room(NULL, NULL, chat->builder);
-    else {
+    } else {
         mx_search_local_rooms(chat->builder, filter_data);
-        req_search_global_rooms(search_name, chat);
+        t_dtp *dtp = mx_search_rooms_request(search);
+        mx_send(chat->out, dtp);
+        mx_free_request(&dtp);
     }
     mx_free_filter_data(filter_data);
 }
 
 void mx_start_search_msgs(GtkSearchEntry *sentry, t_chat *chat) {
-    gchar *search_text = (gchar*)gtk_entry_get_text(GTK_ENTRY(sentry));
-
+    gchar *search = (gchar *) gtk_entry_get_text(GTK_ENTRY(sentry));
     mx_reset_room_for_search(chat->builder);
     mx_clear_found_msgs(chat->builder);
-    if (!strlen(search_text))
+    if (!strlen(search)) {
         mx_stop_search_message(NULL, NULL, chat->builder);
-    else {
+    } else {
         t_groom *groom = mx_get_selected_groom(chat->builder, MX_LOCAL_ROOMS);
-        t_dtp *dtp = mx_search_msgs_request(search_text, groom->id);
-
+        t_dtp *dtp = mx_search_msgs_request(search, groom->id);
         mx_send(chat->out, dtp);
         mx_free_request(&dtp);
-        mx_widget_set_visibility_by_name(chat->builder,
-                                         "stack_messege_rooms", FALSE);
-        mx_widget_set_visibility_by_name(chat->builder,
-                                         "scrlldwnd_found_msgs", TRUE);
-        mx_widget_set_visibility_by_name(chat->builder,
-                                         "label_found_messages", TRUE);
+        mx_widget_set_visibility_by_name(chat->builder, "stack_messege_rooms", FALSE);
+        mx_widget_set_visibility_by_name(chat->builder, "scrlldwnd_found_msgs", TRUE);
+        mx_widget_set_visibility_by_name(chat->builder, "label_found_messages", TRUE);
     }
 }
 
 void mx_start_search_members(GtkSearchEntry *sentry, t_chat *chat) {
-    gchar *search_login = (gchar*)gtk_entry_get_text(GTK_ENTRY(sentry));
-
-    if (!strlen(search_login))
+    gchar *search = (gchar *) gtk_entry_get_text(GTK_ENTRY(sentry));
+    if (!strlen(search)) {
         mx_stop_search_members(NULL, NULL, chat->builder);
-    else
-        mx_search_members(chat->builder, search_login);
+    } else {
+        mx_search_members(chat->builder, search);
+    }
 }
