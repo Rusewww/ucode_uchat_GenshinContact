254a3a102f6fa580369649de9022a01d7c42df05
Ihor Zozulia <izozulia@c1r2p8.khpi.ucode-connect.study>
2022-02-07 13:04:00 +0200

    error_message_authorization

--- client/src/gui/error_messages_authorization.c
+++ client/src/gui/error_messages_authorization.c
@@ -1,22 +1,16 @@
 #include "client.h"
 
 void mx_err_auth_data_handler(GtkBuilder *builder) {
-    GObject *label = gtk_builder_get_object(builder,
-                                            "label_autherror_login");
-
-    gtk_label_set_text(GTK_LABEL(label), MX_ERRMSG_INCCRDATA);
+    GObject *label = gtk_builder_get_object(builder, "label_auth_error_login");
+    gtk_label_set_text(GTK_LABEL(label), "The login or password inccorect");
 }
 
 void mx_err_user_exist_handler(GtkBuilder *builder) {
-    GObject *label = gtk_builder_get_object(builder,
-                                            "label_autherror_signup");
-
-    gtk_label_set_text(GTK_LABEL(label), MX_ERRMSG_USEREXIST);
+    GObject *label = gtk_builder_get_object(builder, "label_auth_error_signup");
+    gtk_label_set_text(GTK_LABEL(label), "User already exist");
 }
 
 void mx_err_cli_exist_handler(GtkBuilder *builder) {
-    GObject *label = gtk_builder_get_object(builder,
-                                            "label_autherror_login");
-
-    gtk_label_set_text(GTK_LABEL(label), MX_ERRMSG_CLIEXIST);
+    GObject *label = gtk_builder_get_object(builder, "label_auth_error_login");
+    gtk_label_set_text(GTK_LABEL(label), "User already authorized");
 }
