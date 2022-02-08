528be2be39ee41d2b2759e393c675c4ecca1c334
Rusewww <rusewww1@gmail.com>
2022-02-08 19:19:37 +0200

    fix

--- client/src/lib/upd_room_power.c
+++ client/src/lib/upd_room_power.c
@@ -3,6 +3,9 @@ gboolean mx_upd_handler(t_dtp *data, t_chat *chat) {
     cJSON *room_id = cJSON_GetObjectItemCaseSensitive(data->json, "room_id");
     cJSON *power = cJSON_GetObjectItemCaseSensitive(data->json, "power");
     gdouble power_d = 0;
+    if(power_d){
+
+    }
     if (chat){
     }
     if (!cJSON_IsNumber(room_id) || !cJSON_IsNumber(power))
